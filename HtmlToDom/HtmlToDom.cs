883767cbb25fe4dec8e96aca955c8f0d7fcc4da7
ginpaydo <y-toyoda@live.jp>
2019-10-13 15:54:05 +0900

    テストデータ作成

--- HtmlToDom/HtmlToDom.cs
+++ HtmlToDom/HtmlToDom.cs
@@ -1,8 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace HtmlToDom
 {
     public class HtmlToDom
     {
+        const string BeginTag = "<";
+        const string EndTag = ">";
+
+        public List<string> SearchAll(string rawText)
+        {
+            var result = new List<string>();
+
+            // 正規表現で変数をチェック
+            //.:任意の文字
+            //+:1回以上の繰り返し
+            //*:0回以上の繰り返し
+            //?: 最小判定（?がなかったら、最も大きな結果を取得する）
+
+            // 何か1文字以上入っていることを条件に検索
+            var terms = BeginTag + "(.+?)" + EndTag;
+            // 条件に合った文字列を全部拾う
+            var r = new Regex(terms, RegexOptions.Multiline);
+            var mc = r.Matches(rawText);
+
+            foreach (var item in mc)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            return result;
+        }
     }
 }
--- HtmlToDomTest/HtmlToDomTest.cs
+++ HtmlToDomTest/HtmlToDomTest.cs
@@ -1,5 +1,6 @@
 using System;
 using Xunit;
+using HtmlToDom;
 
 namespace HtmlToDomTest
 {
@@ -8,6 +9,8 @@ namespace HtmlToDomTest
         [Fact]
         public void Test1()
         {
+            string testData =
+            "<tr class=\"collapse\" id=\"dummy@(item.Id)\"><td colspan=\"3\"><div class=\"d-flex justify-content-end\"><button type=\"button\" class=\"btn btn-primary\" style=\"margin-right:48px\" onclick=\"activate(@item.Id)\">—LŒø‰»</button><button type=\"button\" class=\"btn btn-danger\" style=\"margin-right:48px\" onclick=\"delete(@item.Id)\">íœ</button></div></td></tr>";
 
         }
     }
