59326de261cdcd0622de13995ea938b91d2db3e6
ginpay <y-toyoda@live.jp>
2021-01-03 19:13:44 +0900

    もうちょっとわかりやすく

--- MinteaCore/HtmlToStrap/Nanashi.cs
+++ MinteaCore/HtmlToStrap/Nanashi.cs
@@ -8,6 +8,9 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
+// 欲しい機能
+// ・インデント付きで出力
+
 namespace MinteaCore.HtmlToStrap
 {
     /// <summary>
@@ -38,10 +41,10 @@ namespace MinteaCore.HtmlToStrap
         public async Task Test()
         {
             // -------------------- 基本的な使い方 --------------------
-            //// まずは、スクレイピングするデータを取得します
-            //var sorce = "<button class=\"navbar-toggler\" type=\"button\" dddd=\"eeee\" data-toggle=\"collapse\" data-target=\"#navbarColor01\" aria-controls=\"navbarColor01\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">" +
-            //    "<span class=\"navbar-toggler-icon\">aaaa</span>" +
-            //    "</button>";
+            // まずは、スクレイピングするデータを取得します
+            var sorce = "<button class=\"navbar-toggler\" type=\"button\" dddd=\"eeee\" data-toggle=\"collapse\" data-target=\"#navbarColor01\" aria-controls=\"navbarColor01\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">" +
+                "<span class=\"navbar-toggler-icon\">aaaa</span>" +
+                "</button>";
 
             //// パースします
             //var parser = new HtmlParser();
@@ -56,47 +59,46 @@ namespace MinteaCore.HtmlToStrap
             ////Console.WriteLine(doc.Source.Text);     // 上のsorceそのまま
 
             // -------------------- 試してみたい --------------------
+            // ドキュメントを作成し、テキストを含む別の段落要素を挿入してツリー構造を変更します。
             // https://anglesharp.github.io/docs/Examples.html#simple-document-manipulation
             // -----------------------------------------------------------
 
             // デフォ設定で新しいコンテキスト作成
             var config = Configuration.Default;
-
             var context = BrowsingContext.New(config);
-            var document = await context.OpenAsync(req => req.Content("<button disabled class=\"navbar-toggler\" eeee bbbb=\"cccc\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarColor01\" aria-controls=\"navbarColor01\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">" +
-                "<span class=\"navbar-toggler-icon\">aaaa</span>"));
-            // <html><head></head><body><h1>サンプルソース</h1><p>これはparagraph要素</p></body></html>
 
-            Console.WriteLine("シリアライズします：");
+            // HTML5をパースする
+            var document = await context.OpenAsync(req => req.Content(sorce));
+
+            // 出力してみる
+            Console.WriteLine("対象部品をシリアライズ（html,head,bodyタグが付いた状態で出力）します：");
             Console.WriteLine(document.DocumentElement.OuterHtml);
 
+            // タグを追加して出力してみる
+            // "<p>こっちは新しいparagraph要素です。</p>"を、sorceの内容の次（buttonタグの次）に追加する
+            Console.WriteLine("他の要素を追加して、もう一度シリアライズします：");
             var p = document.CreateElement("p");
             p.TextContent = "こっちは新しいparagraph要素です。";
-
-            Console.WriteLine("他の要素を追加します。");
             document.Body.AppendChild(p);
-
-            Console.WriteLine("もう一度シリアライズします：");
             Console.WriteLine(document.DocumentElement.OuterHtml);
-            // <html><head></head><body><h1>サンプルソース</h1><p>これはparagraph要素</p><p>こっちは新しいparagraph要素です。</p></body></html>
 
-            var button = document.CreateElement("Aaaa");          // ダメ。大文字にしたいのに小文字にされてしまう。設定で直る？
+            // HTML5に無いタグを追加して出力してみる
+            Console.WriteLine("Aaaaというタグを追加してもう一度シリアライズします：");
+            var button = document.CreateElement("Aaaa");          // ダメ。大文字にしたいのに小文字にされてしまう。
             button.TextContent = "ReactStrapのボタン";
-
-            Console.WriteLine("他の要素を追加します。");
+            button.SetAttribute("className", "ananan");     // ダメ。全部小文字にされてしまう…。後で変換かけるしかないのー？？
+            button.SetAttribute("weapon", "sword");
             document.Body.AppendChild(button);
-
-            Console.WriteLine("もう一度シリアライズします：");
             Console.WriteLine(document.DocumentElement.OuterHtml);
 
             // タグ内の隣の要素：NextElementSibling
             // 子要素：Children
-            // bbbb="cccc" の取得方法。
+            // dddd="eeee" の取得方法。
             var btn = document.Body.Children[0];
             var attr = btn.Attributes;
             foreach (var item in attr)
             {
-                Console.WriteLine($"{item.Name} {item.Value}");
+                Console.WriteLine($"{item.Name} {item.Value}"); // これが"dddd"と"eeee"
             }
             // SetAttributeとかで追加削除できる。
 
@@ -105,7 +107,8 @@ namespace MinteaCore.HtmlToStrap
             //disabledは、IsDisabledプロパティに入る。
             // "eeee"はAttributesにSpecified = falseとして入る、Valueは""
 
-            // とりあえず、今回の用途ではタグ名を正しく出力出来ればOKかも。
+            // 後は、入力のHTML5に従って親子関係が同じ構造でタグ出力出来たら作りたいものが作れる
+            // →元データをrootから辿って片っ端から変換かましていけば良さそうな感じ。
 
 
             Console.WriteLine("おわり");
