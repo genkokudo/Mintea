f12dac3da00104513cf6904a096f06a7805b15d7
ginpay <y-toyoda@live.jp>
2021-02-13 14:20:03 +0900

    作った変換ルールはクラスメソッド化しておく

--- MinteaCore/HtmlToStrap/Nanashi.cs
+++ MinteaCore/HtmlToStrap/Nanashi.cs
@@ -72,6 +72,7 @@ namespace MinteaCore.HtmlToStrap
                 "<button class=\"navbar-toggler\" type=\"button\" dddd=\"eeee\" data-toggle=\"collapse\" data-target=\"#navbarColor01\" aria-controls=\"navbarColor01\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">" +
                 "<span class=\"navbar-toggler-icon\">1111</span>" +
                 "</button>" +
+                "<input readonly/>" +
                 "<label>" +
                 "</label>";
 
@@ -120,13 +121,16 @@ namespace MinteaCore.HtmlToStrap
                 {
                     case Rule.TermCategory.SimpleReplacement:
                         // 置換処理
-                        result = result.Replace(rule.SrcTag, rule.DestTag);
+                        result = result.Replace(rule.TargetTag, rule.DestValue);
                         break;
                     case Rule.TermCategory.RemoveAttr:
                         // 何もなし
                         break;
+                    case Rule.TermCategory.RemoveValue:
+                        // 何もなし
+                        break;
                     case Rule.TermCategory.Class:
-                        result = result.Replace(rule.SrcTag, rule.DestTag);
+                        result = result.Replace(rule.TargetTag, rule.DestValue);
                         break;
                     default:
                         break;
@@ -162,7 +166,7 @@ namespace MinteaCore.HtmlToStrap
         /// </summary>
         /// <param name="src">編集元のタグ</param>
         /// <param name="html">このメソッドで編集する対象のタグ</param>
-        /// <param name="newHtml">今回組み立てているHTML</param>
+        /// <param name="newHtml">今回組み立てているHTML、書き換える対象</param>
         /// <param name="rules"></param>
         private void MakeTree(IElement src, IElement html, IDocument newHtml, List<Rule> rules)
         {
@@ -170,72 +174,97 @@ namespace MinteaCore.HtmlToStrap
             // element.InnerHtml: 子要素のHTMLを取得 :<span class="navbar-toggler-icon">aaaa</span>
             // element.OuterHtml: 自分を含むHTMLを取得 :<button class="navbar-toggler" type="button" dddd="eeee" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon">aaaa</span></button>
             // element.TextContent: 下にいくつ子要素があっても、一番下のテキストを表示する。複数子要素がある場合は全部繋げて表示してしまうので注意。子要素がない場合だけ取得するように。:aaaa
+            //newElm.SetAttribute("className", "ananan");       // 全部小文字にされてしまう。後で変換かける
+            //newElm.SetAttribute("weapon", "sword");
 
-            var newElm = newHtml.CreateElement(src.LocalName);      // 要素を作成
-                                                                    //newElm.SetAttribute("className", "ananan");       // 全部小文字にされてしまう。後で変換かける
-                                                                    //newElm.SetAttribute("weapon", "sword");
+            // 要素を作成
+            var dest = newHtml.CreateElement(src.LocalName);
 
+            // 新しいタグに古いタグの内容を移す
             // classを新しい方に移す
-            // ルールの適用をする
-            foreach (var rule in rules) // TODO:分かんないんだけど、メタデータにしてから組み立ての方がよくない？1個ずつIElementに適用して大丈夫？
-            {
-                if (src.LocalName == rule.SrcTag)
-                {
-                    switch (rule.SrcTermCategory)
-                    {
-                        case Rule.TermCategory.SimpleReplacement:
-                            // 何もなし
-                            break;
-                        case Rule.TermCategory.RemoveAttr:
-                            // 要素を削除
-                            src.RemoveAttribute(rule.SrcTermValue);
-                            break;
-                        case Rule.TermCategory.RemoveValue:
-                            // 要素の値を削除（要素指定方法不明）
-                            break;
-                        case Rule.TermCategory.Class:
-                            if (src.ClassList.Contains(rule.SrcTermValue))
-                            {
-                                src.ClassList.Remove(rule.SrcTermValue);
-                                if (src.ClassList.Length == 0)
-                                {
-                                    // class=""は表示しない
-                                    src.ClassName = null;
-                                }
-                            }
-                            break;
-                        default:
-                            break;
-                    }
-                }
-            }
-
             var clsList = new List<string>();
             foreach (var cls in src.ClassList)
             {
                 clsList.Add(cls);
             }
-            newElm.ClassList.Add(clsList.ToArray());
+            dest.ClassList.Add(clsList.ToArray());
 
             // Attrを新しい方に移す
             foreach (var attr in src.Attributes)
             {
-                newElm.SetAttribute(attr.Name, attr.Value);
+                dest.SetAttribute(attr.Name, attr.Value);
             }
 
             // 最下層の場合、テキスト要素を移す
             if (src.Children.Length == 0)
             {
-                newElm.TextContent = src.TextContent;
+                dest.TextContent = src.TextContent;
             }
 
+            // ルールの適用をする:適用対象のルールを集めてから、適用する
+
+            // 対象のルールを集める
+            var targetRules = new List<Rule>();
+            foreach (var rule in rules)
+            {
+                if (src.LocalName == rule.TargetTag)       // TODO:条件ってこれだけじゃないはず。後で追加。
+                {
+                    switch (rule.SrcTermCategory)
+                    {
+                        case Rule.TermCategory.Class:
+                            if (!src.ClassList.Contains(rule.TargetValue))
+                            {
+                                continue;
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                    targetRules.Add(rule);
+                }
+            }
+
+            // ルールを適用する
+            foreach (var rule in targetRules)
+            {
+                switch (rule.SrcTermCategory)
+                {
+                    case Rule.TermCategory.SimpleReplacement:
+                        // 何もなし
+                        break;
+                    case Rule.TermCategory.RemoveAttr:
+                        // 要素を削除
+                        dest.RemoveAttribute(rule.TargetValue);
+                        break;
+                    case Rule.TermCategory.RemoveValue:
+                        // 要素の値を条件に削除（Attrの値は必ず1つなので要素を削除すればよい）
+                        var targets = dest.Attributes.Where(x => x.Value == rule.TargetValue).ToArray();
+                        foreach (var target in targets)
+                        {
+                            dest.RemoveAttribute(target.Name);
+                        }
+                        break;
+                    case Rule.TermCategory.Class:
+                        dest.ClassList.Remove(rule.TargetValue);
+                        if (dest.ClassList.Length == 0)
+                        {
+                            // class=""は表示しない
+                            dest.RemoveAttribute("class");
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+
             // 子要素を追加
-            html.AppendChild(newElm);
+            html.AppendChild(dest);
 
             // 更に子要素を作成する
             foreach (var child in src.Children)
             {
-                MakeTree(child, newElm, newHtml, rules);
+                MakeTree(child, dest, newHtml, rules);
             }
         }
         #endregion
@@ -248,16 +277,19 @@ namespace MinteaCore.HtmlToStrap
         {
             var result = new List<Rule>
             {
-                //new Rule { SrcTag = "div", SrcTermCategory = Rule.TermCategory.Class, SrcTermValue= "row", DestTag = "Row", DestAttr = "" },
                 // とりあえず単純置換
-                new Rule { SrcTag = "button", SrcTermCategory = Rule.TermCategory.SimpleReplacement, SrcTermValue= "",  DestTag = "Button" },
-                new Rule { SrcTag = "\"", SrcTermCategory = Rule.TermCategory.SimpleReplacement, SrcTermValue= "",  DestTag = "'" },
-                // 除去
-                new Rule { SrcTag = "button", SrcTermCategory = Rule.TermCategory.RemoveAttr, SrcTermValue= "dddd" },
-                new Rule { SrcTag = "button", SrcTermCategory = Rule.TermCategory.RemoveValue, SrcTermValue= "#navbarColor01" },
+                Rule.GetSimpleReplacementRule("button", "Button"),
+                Rule.GetSimpleReplacementRule("label", "Label"),
+                Rule.GetSimpleReplacementRule("\"", "'"),
+                Rule.GetSimpleReplacementRule("class", "className"),
 
+                // 除去
+                Rule.GetRemoveAttrRule("button", "dddd"),
+                Rule.GetRemoveAttrByValueRule("button", "#navbarColor01"),
+                Rule.GetRemoveAttrByValueRule("button", "navbarColor01"),
+                
                 // Class
-                new Rule { SrcTag = "div", SrcTermCategory = Rule.TermCategory.Class, SrcTermValue= "row",  DestTag = "Row" }
+                Rule.GetReplaceTagByClass("div", "row", "Row")
             };
 
             return result;
@@ -270,38 +302,30 @@ namespace MinteaCore.HtmlToStrap
     /// </summary>
     public class Rule
     {
-        // classで残ったものはclassNameに変えたい。（単純変換で良いと思う）
-        // 使用した条件は新しいものから削除：<div class='row aaaa'/> は <Row className='aaaa'/> になる。
-
-        /// <summary>
-        /// 対象のタグ:divなど
-        /// </summary>
-        public string SrcTag { get; set; }
-
         /// <summary>
         /// 条件・種類
         /// classなど
         /// "SimpleReplacement"の時は、SrcTagとSrcTermValueで単純置換する
         /// </summary>
-        public TermCategory SrcTermCategory { get; set; }
+        public TermCategory SrcTermCategory { get; set; } = TermCategory.SimpleReplacement;
+
+        /// <summary>
+        /// 対象のタグ:divなど
+        /// </summary>
+        public string TargetTag { get; set; } = string.Empty;
 
         /// <summary>
         /// 条件・値
         /// mt-3 rowなど
+        /// 単純置換の時は不使用
         /// </summary>
-        public string SrcTermValue { get; set; }
+        public string TargetValue { get; set; } = string.Empty;
 
         /// <summary>
-        /// 変換先タグ:Rowなど
+        /// 変換先の値
+        /// 条件に合った部分をこの値に変換する
         /// </summary>
-        public string DestTag { get; set; }
-
-        ///// <summary>
-        ///// 付属変換attr
-        ///// いくつか同じカテゴリごとに溜めてから合成する（複数のRule）
-        ///// className aaaa と、className bbbbがあったら、className='aaaa bbbb'
-        ///// </summary>
-        //public string DestAttr { get; set; }
+        public string DestValue { get; set; } = string.Empty;
 
         /// <summary>
         /// 条件の種類
@@ -321,35 +345,72 @@ namespace MinteaCore.HtmlToStrap
             RemoveAttr,
 
             /// <summary>
-            /// 対象の値を除去する
-            /// TODO:どうやって要素を指定するの？SrcTermCategoryかな？
+            /// 全ての要素に対して
+            /// 要素の値を条件に値を除去する
+            /// 要素は指定しない：とにかくこの値は消すってイメージ（今のところ）
             /// </summary>
             RemoveValue,
 
             /// <summary>
             /// class要素に対する条件
             /// </summary>
-            Class
+            Class,
+
+            // 特定のClassをAttributeに変換する
         }
 
+        #region 条件作成を補助するクラスメソッド
+        /// <summary>
+        /// 全ての変換後、単純に文字を置換する
+        /// </summary>
+        /// <param name="src">置換前文字列</param>
+        /// <param name="dest">置換後文字列</param>
+        /// <returns></returns>
+        public static Rule GetSimpleReplacementRule(string src, string dest)
+        {
+            return new Rule { TargetTag = src, SrcTermCategory = TermCategory.SimpleReplacement, DestValue = dest };
+        }
+
+        /// <summary>
+        /// 特定の名称のAttrを除去するルールを作成する
+        /// </summary>
+        /// <param name="targetTag">対象タグ</param>
+        /// <param name="targetValue">条件となるAttr名</param>
+        /// <returns></returns>
+        public static Rule GetRemoveAttrRule(string targetTag, string targetValue)
+        {
+            return new Rule { TargetTag = targetTag, SrcTermCategory = TermCategory.RemoveAttr, TargetValue = targetValue };
+        }
+
+        /// <summary>
+        /// 値を持ったAttrを除去するルールを作成する
+        /// あまり使わないかも
+        /// </summary>
+        /// <param name="targetTag">対象タグ</param>
+        /// <param name="targetValue">条件となる値</param>
+        /// <returns></returns>
+        public static Rule GetRemoveAttrByValueRule(string targetTag, string targetValue)
+        {
+            return new Rule { TargetTag = targetTag, SrcTermCategory = TermCategory.RemoveValue, TargetValue = targetValue };
+        }
+
+        /// <summary>
+        /// 
+        /// クラス値は削除する
+        /// </summary>
+        /// <param name="targetTag">対象タグ</param>
+        /// <param name="targetValue">条件となるClass値</param>
+        /// <param name="destTag">どのタグに変換するか</param>
+        /// <returns></returns>
+        public static Rule GetReplaceTagByClass(string targetTag, string targetValue, string destTag)
+        {
+            return new Rule { TargetTag = targetTag, SrcTermCategory = TermCategory.Class, TargetValue = targetValue, DestValue = destTag };
+        }
+        #endregion
+
         // TODO:どんな変換があるかよく整理して、決め直すこと！
-        // <button type="button" class="btn btn-danger">Danger!</button>
-        // <Button color='danger'>Danger!</Button>
-        // ・単純置換：こもじになっちゃったのを大文字に。クォーテーションを変える。
-        // ・除去：classの××があれば除去する。[class btn] [type button]
         // ・条件指定してAttrに変換：class="btn btn-danger" を color='danger'にする[class btn-danger attr color danger]
 
-        // TODO:条件の指定方法
-        // <h1>Heading <span class="badge badge-secondary">New</span></h1>
-        // <h1>Heading <Badge color="secondary">New</Badge></h1>
-        // ・タグ変換：条件指定方法は？
-        //   spanで、classがbadgeだったら、Badgeタグにする。classのbadgeは除去。
-        // ・条件指定してAttrに変換：class="badge-secondary"は、color="secondary"にする。[class badge-secondary attr color secondary]
-        //   classとattrではパラメータの数が1個違う。どうやって吸収する？
-        //   今のところattrからの変換はなし？ → Collapseとかだったらあるはず。
-
-        // とりあえず、「単純置換」「除去」「classとattrの変換（除去機能付き、全小文字を大文字入りに変換する機能付き）」「classとタグの変換（左記機能付き）」の4種類？
-        // 条件指定：タグ、class…だけかも？
     }
 
 }
